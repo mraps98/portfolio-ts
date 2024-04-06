ae556176c86420041c27a96a35feee7ac5fdb8c1
Amritpreet Singh <amritpreet98@gmail.com>
2024-04-06 13:47:08 -0500

    feat: update personal statement for 2024

--- src/components/PersonalStatement/index.tsx
+++ src/components/PersonalStatement/index.tsx
@@ -3,21 +3,21 @@ import { FC } from 'react';
 const PersonalStatement: FC = () => (
     <section className="flex flex-col mt-6 gap-y-4 text-lg shadow-lg p-3 bg-gray-50 bg-opacity-90">
         <p className="text-gray-800 justify">
-            I am a passionate full stack web and mobile developer with a
-            Master’s in Applied Computer Science. I have experience with
-            emerging technologies/practices like GraphQL, React Native,
-            Firebase, .NetCore 5, Tailwind, Redux, Thunks, Asynchronous design,
-            Design Patterns, Lazy loading, NextJS SSR, SSG, Latex, Git, AWS
-            (Deployment and DB), Encryption, Hashing, Data Structures,
-            role-based access, responsive design, test-driven development,
-            mobile first approach, SQL, NoSQL and more.
+            I am a passionate full stack web and mobile developer with 4+ years
+            of experience and a Master&apos;s in Applied Computer Science. I
+            have experience with emerging technologies/practices like GraphQL,
+            React Native, Firebase, PHP, .NetCore 5, Tailwind, Redux, Thunks,
+            Asynchronous design, Design Patterns, Docker, Lazy loading, NextJS
+            SSR, SSG, Prismic, Latex, Git, AWS, Encryption, Hashing, Data
+            Structures, role-based access, responsive design, test-driven
+            development, mobile first approach, SQL, NoSQL and more.
         </p>
         <p className="text-gray-800 justify">
-            I believe in not sticking to one language and love exploring C, C++,
-            Flutter, Python Flask, Java Spring, Android Kotlin/Java, Apple
-            Swift, Ruby. I am able to adapt to changes in the development
-            workflow/languages/frameworks as it is a critical and scalable
-            skill.
+            I believe in not sticking to one language and love exploring PHP,
+            NextJS, React, C, C++, Flutter, Python Flask, Java Spring, Android
+            Kotlin/Java, Apple Swift, Ruby. I am able to adapt to changes in the
+            development workflow/languages/frameworks as it is a critical and
+            scalable skill.
         </p>
         <p className="text-gray-800 justify">
             I believe team work is critical for success and am able to work well
