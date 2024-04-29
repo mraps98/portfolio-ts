5d10fc9cd7d76f130834611582dc881349064f45
Amritpreet Singh <amritpreet98@gmail.com>
2024-04-29 12:54:38 -0500

    chore(personal statement): improve & polish

--- src/components/PersonalStatement/index.tsx
+++ src/components/PersonalStatement/index.tsx
@@ -3,29 +3,19 @@ import { FC } from 'react';
 const PersonalStatement: FC = () => (
     <section className="flex flex-col mt-6 gap-y-4 text-lg shadow-lg p-3 bg-gray-50 bg-opacity-90">
         <p className="text-gray-800 justify">
-            I am a passionate Full stack Software Engineer with experience in
-            all layers of Enterprise Applications and a Master&apos;s in Applied
-            Computer Science with a GPA of 3.9. I have experience with emerging
-            technologies/practices like GraphQL, React Native, Firebase, PHP,
-            .NetCore, Tailwind CSS, Redux, Thunks, Asynchronous design, Design
-            Patterns, Docker, Lazy loading, NextJS SSR, SSG, Prismic, Latex,
-            Git, AWS, Encryption, Hashing, Data Structures, role-based access,
-            responsive design, test-driven development, mobile first approach,
-            SQL, NoSQL and more.
-        </p>
-        <p className="text-gray-800 justify">
-            I believe in not sticking to one language and love exploring PHP,
-            NextJS, React, C, C++, Flutter, Python Flask, Java Spring, Android
-            Kotlin/Java, Apple Swift, Ruby. I am able to adapt to changes in the
-            development workflow/languages/frameworks as it is a critical and
-            scalable skill.
-        </p>
-        <p className="text-gray-800 justify">
-            I believe team work is critical for success and am able to work well
-            with individuals with a broad range of traits. I am able to deduce
-            business decisions and solutions rapidly and independently. I
-            believe an eager-to-learn attitude is the only way forward and that
-            drives me.
+            Innovative and results-driven Full Stack Software Engineer with a
+            proven track record in developing and deploying cutting-edge
+            solutions across various industries. With extensive experience in
+            rebranding and revitalizing software suites for widespread adoption,
+            I specialize in driving impactful projects from conception to
+            delivery. Skilled in leveraging advanced technologies to streamline
+            processes, I have successfully implemented AI-driven
+            functionalities, integrated with industry-leading platforms, and
+            maintained high-traffic applications serving hundreds of thousands
+            of users. Holding a master's degree with 3.9 GPA, I combine academic
+            excellence with practical expertise to deliver exceptional results.
+            Eager to collaborate on transformative projects and contribute to
+            the forefront of software innovation.
         </p>
     </section>
 );
